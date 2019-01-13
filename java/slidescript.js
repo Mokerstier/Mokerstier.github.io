8d06d463c01e9d828f72f1b890cdba40a624d493
Mokerstier <33430655+Mokerstier@users.noreply.github.com>
2019-01-13 13:35:35 +0100

    Verhaal, mijn verhalen, carousel
    carousel zo goed als af,
    mijn verhalen pagina toegevoegd,
    styling mijn verhaal

--- java/slidescript.js
+++ java/slidescript.js
@@ -1,35 +1,61 @@
 /*jslint browser: true, devel: true, eqeq: true, plusplus: true, sloppy: true, vars: true, white: true*/
 /*eslint-env browser*/
 /*eslint 'no-console':0*/
+var carouselBox = document.querySelector('.carous');
 var slideIndex = 1;
-var volgende = document.querySelector('volgende');
-var vorige = document.querySelector('vorige');
-var bolletje = document.querySelectorAll('dot');
+var slides = document.querySelectorAll('.carous article');
+var millis = 5000;
+var delay;
+var dots = document.querySelectorAll('.dot');
 
-function showSlides(n) {
-  var i;
-  var slides = document.getElementsByClassName("mySlides");
-  var dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1;}    
-  if (n < 1) {slideIndex = slides.length;}
-  for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";  
-  }
-  for (i = 0; i < dots.length; i++) {
-      dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex-1].style.display = "block";  
-  dots[slideIndex-1].classList.add ('active');
+function carousel(){
+    var i;
+    
+    for (i=0; i < slides.length; i++){
+        slides[i].style.display = "none";
+        
+    }
+    
+    if (slideIndex > slides.length) {
+        slideIndex = 1;
+    }
+    for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", " inactive");
+      
+    }
+    slides[slideIndex-1].style.display = "block";
+    dots[slideIndex - 1].classList.add('active');
+    dots[slideIndex - 1].classList.remove('inactive');
 }
-showSlides(slideIndex);
-
-function plusSlides(n) {
-  showSlides(slideIndex += n);
+function nextSlide(){
+    carousel();
+    slideIndex++;
 }
-
 function currentSlide(n) {
-  showSlides(slideIndex = n);
+    clearInterval(delay);
+    slideIndex = n + 1;
+    nextSlide();
+    delay = setInterval(nextSlide, millis);
+  }
+function pauseCaro(){
+    clearInterval(delay, millis);
+    
 }
-bolletje.addEventListener("click", currentSlide([]+1));
-volgende.addEventListener("click", plusSlides(1));
-vorige.addEventListener("click", plusSlides(-1));
+
+function startSlides(){
+    pauseCaro();
+    nextSlide();
+    delay = setInterval(nextSlide, millis);
+}
+
+startSlides();
+
+dots.forEach(function (dots, index){
+    dots.addEventListener("click", function(){
+     console.log(dots + index);
+        currentSlide(index);
+    });
+});
+
+carouselBox.addEventListener("mouseenter", pauseCaro);
+carouselBox.addEventListener("mouseleave", startSlides);
