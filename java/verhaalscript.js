1294f6ddd277e4f1f9ddfb5f751919d7c8f12323
Mokerstier <33430655+Mokerstier@users.noreply.github.com>
2019-01-16 00:19:11 +0100

    laatste hand verhaal + nog veel meer!

--- java/verhaalscript.js
+++ java/verhaalscript.js
@@ -10,6 +10,12 @@ var eerste = document.getElementById("eerste");
 var tweede = document.getElementById("tweede");
 var derde = document.getElementById("derde");
 var vierde = document.getElementById("vierde");
+var buttonBewaar = document.querySelector('footer button:last-of-type');
+var downloadVerhaal = document.querySelector('footer button:first-of-type');
+var bewaardeVerhalen = document.getElementById('bewaarCount');
+var gedownloadeVerhalen = document.getElementById('downloadCount');
+var aantalBewaard = 0; //variabele voor aantal bewaarde verhalen
+var aantalDownload = 0;
 
 function myScroll1() {
          
@@ -72,6 +78,81 @@ function scrollIndicator() {
         prompt.classList.add('verstop');
     }
 }
+function bolletje(){
+    var aBaD = (aantalBewaard + aantalDownload);
+    aBaDTotaal.innerHTML = aBaD;
+    console.log(aBaDTotaal);
+    if (aBaD < 1){
+        aBaDTotaal.classList.remove('added');
+    } else {
+        aBaDTotaal.classList.add('added');
+    }
+}
+// ===== Micro bewaren =====
+
+
+    buttonBewaar.addEventListener("click", function () {
+        console.log("je klikt button nummer "); 
+
+        if (buttonBewaar.classList == ('bewaard') === true) {
+            clearTimeout(displayTime); // als de gebruiker 2 keer achter elkaar klikt op de button wordt de animatie van toevoegen niet gestart
+            aantalBewaard--;
+            bewaardeVerhalen.innerHTML = aantalBewaard;
+            bolletje();
+            buttonBewaar.classList.remove('bewaard'); // haalt 'bewaard' class weg
+
+        } else {
+            displayTime = setTimeout(function () { // Laat lijstje en header zien
+                loginLijstje.classList.add('display');
+                header.classList.add('bewaren');
+                aantalBewaard++;
+                console.log(aantalBewaard);
+
+                console.log(bewaardeVerhalen);
+                bewaardeVerhalen.innerHTML = aantalBewaard;
+                
+
+            }, 1000);
+            hideTime = setTimeout(function () { // Haalt lijstje en header weer weg
+               bolletje(); loginLijstje.classList.remove('display');
+                header.classList.remove('bewaren');
+            }, 3000);
+            displayTime; // Activeerd display functie na 1 sec
+            hideTime; // Activeerd hide functie na 3 sec
+            
+            buttonBewaar.classList.add('bewaard'); // geeft de button class 'bewaard'  
+        }
+    });
+
+console.log(bewaardeVerhalen);
+// Micro downloaden
+
+     function downLoad() {
+        console.log("klikie");
+        if (downloadVerhaal.classList.contains('inactive') === true){
+            aantalDownload--;
+            gedownloadeVerhalen.innerHTML = aantalDownload;
+            downloadVerhaal.innerHTML = 'Download';
+            downloadVerhaal.classList.add('active');
+            downloadVerhaal.classList.remove('inactive');
+            bolletje();
+        } else {
+            downloadVerhaal.classList.add('download');
+        
+        aantalDownload++;
+        gedownloadeVerhalen.innerHTML = aantalDownload;
+        downloadVerhaal.innerHTML = '';
+        setTimeout(function(){
+           bolletje(); 
+           downloadVerhaal.classList.remove('download', 'active');
+           downloadVerhaal.innerHTML = 'Gedownload';
+            
+           downloadVerhaal.classList.add('inactive');
+        }, 5000);
+        }
+    }
+
+downloadVerhaal.addEventListener("click", downLoad);
 
 window.onscroll = function() {scrollIndicator();};
 
